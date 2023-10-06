00164936c674e9227240a483b18140e10eebf48d
Ckk3 <contato.gustavomota@gmail.com>
2023-10-06 12:40:27 -0300

    create all dockerfiles and prepare app to production

--- ckkwebsite/ckkwebsite/settings.py
+++ ckkwebsite/ckkwebsite/settings.py
@@ -15,10 +15,15 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = ["nginx"]
+
 DEBUG = env("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS.append("localhost")
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
 
 
 # Application definition
